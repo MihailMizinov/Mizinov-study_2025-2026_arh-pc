5d29c840518c6b8897cf065367e5552593763583
MihailMizinov <159151026+MihailMizinov@users.noreply.github.com>
2025-11-15 11:45:57 +0300

    Add files via upload

---
+++ labs/lab6/samos.asm
@@ -0,0 +1,29 @@
+%include 'in_out.asm'
+
+SECTION .data
+msg: DB 'Введите x',0
+
+SECTION .bss
+x: RESB 80
+SECTION .text
+GLOBAL _start
+_start:
+mov eax, msg
+call sprintLF
+
+mov ecx, x
+mov edx, 80
+call sread
+
+mov eax,x ; вызов подпрограммы преобразования
+call atoi ; ASCII кода в число, `eax=x`
+
+mov ebx,2
+mul ebx
+add eax,10
+xor edx, edx
+mov ebx,3
+div ebx
+
+call iprintLF
+call quit
